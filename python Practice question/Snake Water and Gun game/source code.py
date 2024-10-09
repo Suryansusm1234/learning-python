fb8a186c22f80556ad9c6bae40b8499dcfb53292
Suryansu sanjeebani Mohanty <ssanjeebanimohanty@gmail.com>
2024-10-09 22:18:48 +0530

    Created a new snake water and gun game

---
+++ python Practice question/Snake Water and Gun game/source code.py
@@ -0,0 +1,39 @@
+import random
+computer = ["s", "W","G"]
+Name = input("Enter your Name:-")
+count =0
+tie = 0
+lost = 0
+for i in range(5):
+    def play():
+        user = input("What's your choice? 's' for snake, 'w' for water, 'g' for gun:  ").lower()
+        if user not in ["s", "w", "g"]:
+            raise ValueError("Invalid input. Please enter 's', 'w', or 'g'.")
+        computer_choice = random.choice(computer).lower()
+    
+        if user == computer_choice:
+            print(f"The computer choiced:-{computer_choice}")
+            global tie
+            tie+=1
+            return 'It\'s a tie'
+    
+    # r > s, s > w, w > g
+        if is_win(user, computer_choice):
+            print(f"The computer choiced:-{computer_choice}")
+            global count
+            count+=1
+            return 'You won!'
+    
+        return print(f"The computer choiced:-{computer_choice}",'You lost!')
+    def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > w, w > g
+        if (player == 's' and opponent == 'w') or (player == 'w' and opponent == 'g') or (player == 'g' and opponent == 's'):
+            return True
+        else:
+            global lost
+            lost+=1
+
+    print(play())
+print(f"{Name} won {count} and there is {tie} tie and there is {lost} lost out of 5")
+
