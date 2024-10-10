e414f11f1700c3fb135e5194c567b94a891d862d
Suryansu sanjeebani Mohanty <ssanjeebanimohanty@gmail.com>
2024-10-10 12:06:44 +0530

    Updated the fibonacci Series

--- Learning/fibonacci Series.py
+++ Learning/fibonacci Series.py
@@ -1,7 +1,8 @@
-def fib (num):
-    if num == 1 or num == 0 :
-        return 1
-    else:
-        return fib(num-1)+ fib(num -2)
-a = int(input("Enter A number:-"))
-print(fib(a))
+n =int(input("Enter the Range upto which you want to calculate the fibonacci Series:-"))
+for i in range(n):
+    def fib (num):
+        if num == 1 or num == 0 :
+            return 1
+        else:
+            return fib(num-1)+ fib(num -2)
+    print(fib(i) ,end=(","))
