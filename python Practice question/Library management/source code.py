b6be7423dedecf56b8a6fcad3bcf0b5de35ad14c
Suryansu sanjeebani Mohanty <ssanjeebanimohanty@gmail.com>
2024-10-14 14:53:04 +0530

    Updated the source code of the library Management

--- python Practice question/Library management/source code.py
+++ python Practice question/Library management/source code.py
@@ -1,6 +1,6 @@
 import datetime
 with open("Booklist.txt",'r') as f:
-    book_list = [f.read().splitlines()]
+    book_list = [line.strip() for line in f.readlines()]
 class library:
     def __init__(self):
         pass
@@ -10,30 +10,39 @@ class library:
             if book_name in book_list:
                 print("Book already exists")
             else:
-                f.write(book_name)
+                book_list.append(book_name)
+                f.write(book_name + "\n")
                 print("Book added successfully")
     def borrow_book(self):
         book_name= input("Enter the name of the book You want to Borrow:")
         if book_name in book_list:
             print("The Book is Available and You can Borrow it")
             book_list.remove(book_name)
+            with open("Booklist.txt",'w') as f :
+                for i in book_list:
+                    f.write(i + "\n")
         else:
             print("Sorry! The book is not available and You can not Borrow it")
     def return_book(self):
         book_name= input("Enter the name of the book You want to return:")
-        if book_name in book_list:
-            date = input("Enter the date of borrow")
+        if book_name not in book_list:
+            date = input("Enter the date of borrow (year-month-date):-")
+            borrow_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
             current_date =datetime.date.today()
-            difference = current_date - date
+            difference = current_date - borrow_date
             if difference >= datetime.timedelta(days=30):
-                fine = (difference - 30)*10
-                print(f"You have to pay a fine of {fine} as You have not returend thebook in time")
-            print("The Book is Available and You can return it along with the fine")
+                fine = (difference - datetime.timedelta(days=30)).days * 10
+                print(f"You have to pay a fine of {fine} as You have not returend the book in time")
+            print("The Book is not Available and You can return it along with the fine")
             book_list.append(book_name)
+            with open('Booklist.txt', 'w') as f:
+                for i in book_list:
+                    f.write(i + "\n")
+            print("Book Added successfully")
         else:
-            print("Sorry! The book is not available and You can not return it")
+            print("Sorry! The book is available and You can not return it")
     def check(self):
-        book_name = input("Enter the name of the book You wantto check")
+        book_name = input("Enter the name of the book You wantto check:")
         if book_name in book_list:
             print("The Book is Available")
         else:
